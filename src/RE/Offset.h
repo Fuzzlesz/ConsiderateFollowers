caac3fb1e3d0c65b819bc211787985bd964bc197
Fuzzles <fuzzmyrim@gmail.com>
2024-11-16 11:24:54 -0800

    1.2.1 update
    rebased branch onto 1.2.2

--- src/RE/Offset.h
+++ src/RE/Offset.h
@@ -6,31 +6,31 @@ namespace RE
 	{
 		namespace AIProcess
 		{
-			constexpr auto ClearGreetingInfoData = REL::ID(39317);
-			constexpr auto ProcessGreet = REL::ID(39162);
+			constexpr auto ClearGreetingInfoData = REL::ID(38338);
+			constexpr auto ProcessGreet = REL::ID(38202);
 		}
 
 		namespace Character
 		{
 			constexpr auto IsTalking = REL::ID(36277);
-			constexpr auto GetProccess = REL::ID(37965);
-			constexpr auto GetEssential = REL::ID(37228);
+			constexpr auto GetProccess = REL::ID(36940);
+			constexpr auto GetEssential = REL::ID(36246);
 		}
 
 		namespace DialogueItem
 		{
-			constexpr auto FirstResponse = REL::ID(35222);
-			constexpr auto RunResult = REL::ID(35225);
+			constexpr auto FirstResponse = REL::ID(34415);
+			constexpr auto RunResult = REL::ID(34418);
 		}
 
 		namespace PlayerCharacter
 		{
-			constexpr auto GetSleepRestState = REL::ID(40413);
+			constexpr auto GetSleepRestState = REL::ID(39342);
 		}
 
 		namespace TESTopic
 		{
-			constexpr auto CreateDialogueItem = REL::ID(25541);
+			constexpr auto CreateDialogueItem = REL::ID(25014);
 		}
 	}
 }
